69a7d64f3ec251ffa193f9b5c27aca3d01339f40
VincentDS <de_vinnie111_rulezzz@hotmail.com>
2015-09-16 10:12:40 +0200

    renderd waits on osm2pgsql before starting

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Wait for osm-bright-end
 
-while [ ! -f /var/lib/start/osm-bright-end ]
+while [ ! -f /var/lib/start/osm-bright-end ] || [ ! -f /var/lib/start/osm2pgsql-end ]
 do
-  echo "renderd still waiting for osm-bright to complete.."
+  echo "renderd still waiting for osm2pgsql & osm-bright to complete.."
   sleep 2
 done
 
 #start the daemon
-sleep 60
+sleep 10
 echo "renderd is starting"
 exec $START_CMD
